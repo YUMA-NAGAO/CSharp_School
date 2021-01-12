fcccb231ba8da70d92945de06ee0234b3c982df9
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2021-01-12 20:32:12 +0900

    [add]C#の超入門4-2を追加

---
+++ StudyClass/02PracticeCSharp/02SampleRPG/02SampleRPG/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02SampleRPG
+{
+    class Player
+    {
+        public string name;
+        public int hp;
+
+        public Player(string name, int hp)
+        {
+            this.name = name ;
+            this.hp = hp;
+        }
+
+        public void Attack()
+        {
+            Console.WriteLine(this.name + "は攻撃した");
+        }
+
+        public void Defence()
+        {
+            Console.WriteLine(this.name + "は防御した");
+        }
+    }
+}
---
+++ StudyClass/02PracticeCSharp/02SampleRPG/02SampleRPG/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _02SampleRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hp = { 420, 120, 6000, 0, 1200 };
+
+            int[] newHP = hp.Where(n => n >= 500).ToArray<int>();
+
+            foreach (int n in newHP)
+            {
+                Console.WriteLine(n);
+            }
+
+            Player player = new Player("ひろし", 100);
+
+            Console.WriteLine(player.name + "体力は" + player.hp);
+        
+        }
+    }
+}
