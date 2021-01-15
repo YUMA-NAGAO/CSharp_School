c9f8a6dabc665c9a2da45864cb58a8aa224ff351
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2021-01-15 23:17:01 +0900

    [add]BinarySerachの練習を追加

---
+++ StudyClass/02PracticeCSharp/06ListBinarySerach/06ListBinarySerach/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06ListBinarySerach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> l = new List<double>();
+            l.Add(10);
+            l.Add(5);
+            l.Add(2);
+            l.Sort();
+            //l.Reverse();
+            Console.WriteLine(l[0]);
+            Console.WriteLine(l.BinarySearch(10));
+        }
+    }
+}
