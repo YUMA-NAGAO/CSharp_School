9cc0de9d634037a750a715c6e19dc83f71229be2
yuma <acfoapon@gmail.com>
2021-01-24 15:11:18 +0900

    [edit]消費税計算の計算機の実装

---
+++ StudyClass/02PracticeCSharp/02WindowsApp/02Culculate/02Culculate/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace _02Culculate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.taxPriceBox = new System.Windows.Forms.TextBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "税込み前";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "税込み後";
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(121, 105);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(208, 20);
+            this.priceBox.TabIndex = 2;
+            // 
+            // taxPriceBox
+            // 
+            this.taxPriceBox.Enabled = false;
+            this.taxPriceBox.Location = new System.Drawing.Point(121, 238);
+            this.taxPriceBox.Name = "taxPriceBox";
+            this.taxPriceBox.Size = new System.Drawing.Size(208, 20);
+            this.taxPriceBox.TabIndex = 2;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(145, 335);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(183, 23);
+            this.calcButton.TabIndex = 3;
+            this.calcButton.Text = "計算する！！";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.Caluclate);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(345, 450);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.taxPriceBox);
+            this.Controls.Add(this.priceBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.TextBox taxPriceBox;
+        private System.Windows.Forms.Button calcButton;
+    }
+}
+
