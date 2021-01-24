a3432067d178b5a9ce29f4779920a89460d7dd26
yuma <acfoapon@gmail.com>
2021-01-24 15:46:23 +0900

    [edit]税率計算機の作成

--- StudyClass/02PracticeCSharp/02WindowsApp/02Culculate/02Culculate/Form1.cs
+++ StudyClass/02PracticeCSharp/02WindowsApp/02Culculate/02Culculate/Form1.cs
@@ -19,7 +19,18 @@ namespace _02Culculate
 
         private void Caluclate(object sender, EventArgs e)
         {
+            int price;
+            bool success = int.TryParse(this.priceBox.Text, out price);
 
+            if (success)
+            {
+                int taxPrice = (int)(price * 1.1);
+                this.taxPriceBox.Text = taxPrice.ToString();
+            }
+            else
+            {
+                MessageBox.Show("数字を入力してください");
+            }
         }
     }
 }
