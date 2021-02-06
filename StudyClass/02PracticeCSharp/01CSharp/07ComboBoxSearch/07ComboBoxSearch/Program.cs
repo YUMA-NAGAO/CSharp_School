bd02c1186d677ebad3ed674ff4138d192a688e7c
yuma <acfoapon@gmail.com>
2021-02-07 08:17:52 +0900

    [add]コンボボックスの後からデータの追加方法について

---
+++ StudyClass/02PracticeCSharp/01CSharp/07ComboBoxSearch/07ComboBoxSearch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _07ComboBoxSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = @"\\Mac\Home\Downloads\GitHub\Lottery\Lottery\Lottery\ClassDataCSV";
+            string[] files = Directory.GetFiles(dir, "*.csv");
+            string[] data= { };
+            int a = 0;
+            foreach (string s in files)
+            {
+
+                Array.Resize(ref data, data.Length + 1);
+                data[a] = s.Remove(0, 65);
+                a += 1;
+
+            }
+            foreach (string s in data)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
---
+++ StudyClass/02PracticeCSharp/02WindowsApp/06ComboAdd/06ComboAdd/AddCombo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _06ComboAdd
+{
+    class AddCombo
+    {
+        public string[] DirectroyData()
+        {
+            string dir = @"\\Mac\Home\Downloads\GitHub\Lottery\Lottery\Lottery\ClassDataCSV";
+            string[] files = Directory.GetFiles(dir, "*.csv");
+            string[] data = { };
+            int a = 0;
+            foreach (string s in files)
+            {
+
+                Array.Resize(ref data, data.Length + 1);
+                data[a] = s.Remove(0, 65);
+                a += 1;
+
+            }
+            foreach (string s in data)
+            {
+                Console.WriteLine(s);
+            }
+            return data;
+        }
+    }
+}
+
+
