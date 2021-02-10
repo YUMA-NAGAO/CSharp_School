ffdfdedba69939989d649a35dd54f480a38d8d32
YUMA-NAGAO <55423883+YUMA-NAGAO@users.noreply.github.com>
2021-02-10 18:07:16 +0900

    [edit]映像は取得できたが、ボタンクリック後にエラーが発生する

--- StudyClass/02PracticeCSharp/02WindowsApp/08OpenCvSharp/08OpenCvSharp/Form1.Designer.cs
+++ StudyClass/02PracticeCSharp/02WindowsApp/08OpenCvSharp/08OpenCvSharp/Form1.Designer.cs
@@ -30,6 +30,9 @@ namespace _08OpenCvSharp
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -42,14 +45,30 @@ namespace _08OpenCvSharp
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(58, 71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 284);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            //this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,6 +76,8 @@ namespace _08OpenCvSharp
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- StudyClass/02PracticeCSharp/02WindowsApp/08OpenCvSharp/08OpenCvSharp/Form1.cs
+++ StudyClass/02PracticeCSharp/02WindowsApp/08OpenCvSharp/08OpenCvSharp/Form1.cs
@@ -1,28 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenCvSharp;
+using OpenCvSharp.Internal;
 
 namespace _08OpenCvSharp
 {
     public partial class Form1 : Form
     {
+        int WIDTH = 640;
+        int HEIGHT = 480;
+        Mat frame;
+        VideoCapture capture;
+        Bitmap bmp;
+        Graphics graphic;
+        DateTime dt = DateTime.Now;
         public Form1()
         {
             InitializeComponent();
+            //カメラ画像取得用のVideoCapture作成
+            capture = new VideoCapture(0);
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("camera was not found!");
+                this.Close();
+            }
+            capture.FrameWidth = WIDTH;
+            capture.FrameHeight = HEIGHT;
+
+            //取得先のMat作成
+            frame = new Mat(HEIGHT, WIDTH, MatType.CV_8UC3);
+
+            //表示用のBitmap作成
+            bmp = new Bitmap(frame.Cols, frame.Rows, (int)frame.Step(), System.Drawing.Imaging.PixelFormat.Format24bppRgb, frame.Data);
+
+            //PictureBoxを出力サイズに合わせる
+            pictureBox1.Width = frame.Cols;
+            pictureBox1.Height = frame.Rows;
+
+            //描画用のGraphics作成
+            graphic = pictureBox1.CreateGraphics();
+
+            //画像取得スレッド開始
+            backgroundWorker1.RunWorkerAsync();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            using (Mat mat = new Mat("test.jpg"))
+            //スレッドの終了を待機
+            backgroundWorker1.CancelAsync();
+            while (backgroundWorker1.IsBusy)
+                Application.DoEvents();
+        }
+
+        public void button1_Click(Object sender,EventArgs e)
+        {
+            capture.Grab();
+            NativeMethods.videoio_VideoCapture_operatorRightShift_Mat(capture.CvPtr, frame.CvPtr);
+
+
+            Timer t = new Timer();
+            t.Interval = 40;
+            t.Tick += new EventHandler(ShowImage);
+            t.Start();
+            
+        }
+        private void ShowImage(Object sender, EventArgs e)
+        {
+            string filename = dt.ToString("HH:mm:ss");
+            using (frame)
             {
-                Cv2.ImShow("sample_show", mat);
+                Cv2.ImShow(filename, frame);
             }
         }
     }
